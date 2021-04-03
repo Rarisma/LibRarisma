ea73f6d6f1d7810a24476b350070db1445e5e51e
Rarisma <49795711+Rarisma@users.noreply.github.com>
2021-04-03 15:33:56 +0100

    Update to 1.2

---
+++ IO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.IO.Compression;
+namespace LibRarisma
+{
+    public class IO
+    {
+        public static string DownloadFile(string URL, string Directory, string Filename)
+        {
+            try // This will download the resources.zip
+            {
+                using (var client = new System.Net.WebClient()) { client.DownloadFile(URL, Directory + "//" + Filename); }
+            }
+            catch { return "Error Code 1\nFailed to download file?\nAre you connected to the internet?"; } //This should only happen if the user cannot access github
+
+            return "Success!";
+        }
+    }
+}
---
+++ Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibRarisma
+{
+    public static class Math
+    {
+        public static Random RandInt = new Random();
+
+        public static Int64 Mean(List<Int32> ListOfNumbers) { return ListOfNumbers.Sum() / ListOfNumbers.Count(); }
+
+        /// <summary> Gets the Range of a set of numbers </summary>
+        public static Int64 Range(List<Int32> ListOfNumbers) { return ListOfNumbers.Max() - ListOfNumbers.Min(); }
+
+        public static double MakePostive(double input) { if (input < 0) { return input * -1; } else { return input; } }
+
+        public static double MakeNegative(double input) { if (input > 0) { return input * -1; } else { return input; } }
+
+        public static Int64 RandomInt(Int32 Number0, Int32 Number1) { return RandInt.Next(Number0, Number1); }
+
+    }
+}
